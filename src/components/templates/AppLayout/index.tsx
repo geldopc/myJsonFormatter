3747372afbd2e298f2eb68aeed78ff69081687ff
Geldo Pina Costa <geldopc@gmail.com>
2026-06-15 21:28:28 -0300

    chore: remove unused Navbar component

--- src/components/templates/AppLayout/index.tsx
+++ src/components/templates/AppLayout/index.tsx
@@ -1,15 +1,9 @@
 import { Outlet } from "react-router-dom";
-import { Navbar } from "@modules/Navbar";
 
-type AppLayoutProps = {
-  title?: string;
-};
-
-export function AppLayout({ title }: AppLayoutProps) {
+export function AppLayout() {
   return (
     <div id="app-layout" className="grain min-h-screen bg-background text-foreground">
-      <Navbar title={title} />
-      <main id="main-content" className="pt-14">
+      <main id="main-content">
         <Outlet />
       </main>
     </div>
