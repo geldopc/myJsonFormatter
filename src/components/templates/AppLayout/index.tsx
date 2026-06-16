e1e7e6a82ef1e41cab704a9871f157f500702bdb
Geldo Pina Costa <geldopc@gmail.com>
2026-06-15 21:32:23 -0300

    fix: AppLayout must be h-screen flex flex-col for full-screen editor layout

--- src/components/templates/AppLayout/index.tsx
+++ src/components/templates/AppLayout/index.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
   return (
-    <div id="app-layout" className="grain min-h-screen bg-background text-foreground">
-      <main id="main-content">
+    <div id="app-layout" className="grain h-screen bg-background text-foreground flex flex-col">
+      <main id="main-content" className="flex-1 overflow-hidden flex flex-col">
         <Outlet />
       </main>
     </div>
