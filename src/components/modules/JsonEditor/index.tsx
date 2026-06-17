f4debde60f6f11636995e637a983b784d1b1dd46
Geldo Pina Costa <geldopc@gmail.com>
2026-06-17 10:22:03 -0300

    feat: add JsonEditor module wrapping CodeMirror
    Co-Authored-By: @geldopc

---
+++ src/components/modules/JsonEditor/index.tsx
@@ -0,0 +1,34 @@
+import { json } from "@codemirror/lang-json";
+import { search } from "@codemirror/search";
+import type { EditorView } from "@codemirror/view";
+import CodeMirror from "@uiw/react-codemirror";
+import { editorTheme } from "@/utils/codemirror/theme";
+
+interface JsonEditorProps {
+  value: string;
+  onChange: (value: string) => void;
+  onCreateEditor: (view: EditorView) => void;
+}
+
+export function JsonEditor({ value, onChange, onCreateEditor }: JsonEditorProps) {
+  return (
+    <div id="json-editor" className="flex-1 overflow-hidden">
+      <CodeMirror
+        value={value}
+        onChange={onChange}
+        onCreateEditor={onCreateEditor}
+        extensions={[json(), search({ top: true }), editorTheme]}
+        basicSetup={{
+          searchKeymap: false,
+          highlightSelectionMatches: false,
+          foldGutter: false,
+          highlightActiveLine: false,
+          highlightActiveLineGutter: false,
+          lineNumbers: true,
+        }}
+        height="100%"
+        className="h-full"
+      />
+    </div>
+  );
+}
